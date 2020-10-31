7a1184266c2b7a0f99c3409612e5ad3fc6972926
derheld42 <carter.smithhart@gmail.com>
2020-10-31 14:18:32 -0700

    Add servo and light controls

--- main.ts
+++ main.ts
@@ -1,5 +1,23 @@
+input.onButtonPressed(Button.A, function () {
+    LIGHT += 1
+})
+input.onButtonPressed(Button.B, function () {
+    if (SERVO == 0) {
+        SERVO = 180
+    } else {
+        SERVO = 0
+    }
+})
+let LIGHT = 0
+let SERVO = 0
 radio.setGroup(1065)
+SERVO = 180
 basic.forever(function () {
     radio.sendValue("pitch", input.rotation(Rotation.Pitch))
     radio.sendValue("roll", input.rotation(Rotation.Roll))
+    if (LIGHT == 3) {
+        LIGHT = 0
+    }
+    radio.sendValue("light", LIGHT)
+    radio.sendValue("serveo", SERVO)
 })
